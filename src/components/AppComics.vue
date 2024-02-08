f70b61bc84cb01c23e1e676f229aade6e551c495
Mattia-Giunta <mattia.giunta.90@hotmail.com>
2024-02-08 17:43:30 +0100

    aggiunto file AppComic come figlio di MainApp e svilupopato le immagini con i fumetti con ciclo v-for e assegnato i props

---
+++ src/components/AppComics.vue
@@ -0,0 +1,52 @@
+<script>
+// import
+
+
+export default {
+  name: "AppComics",
+
+  props:{
+    propsSrc: String,
+    propsSeries: String,
+
+  }
+};
+
+</script>
+
+
+
+<template>
+
+<div  class="list">
+    <figure class="list-figure">
+
+    <img :src="propsSrc" alt="propsSeries">
+
+    </figure>
+
+    <h4>{{ propsSeries }}</h4>
+</div>
+
+
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/partials/variables" as *;
+@use "../styles/partials/mixins" as *;
+
+
+.list-figure{
+    aspect-ratio: 1;
+    overflow: hidden;
+    margin-bottom: 10px;
+
+    img{
+    width: 100%;
+      
+    }
+    
+}
+
+
+</style>
--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -1,8 +1,106 @@
 <script>
-// import
+
+import AppComics from '../components/AppComics.vue'
 
 export default {
+
   name: "MainApp",
+
+  components:{
+    AppComics,
+  },
+
+  data() {
+
+    return {
+
+      listComics: [
+        {
+          thumb: "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
+          price: "$19.99",
+          series: "Action Comics",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg",
+          price: "$3.99",
+          series: "American Vampire 1976",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
+          price: "$16.99",
+          series: "Aquaman",
+          type: "graphic novel",
+        },
+        {
+          thumb: "https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg",
+          price: "$2.99",
+          series: "Batgirl",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg",
+          price: "$3.99",
+          series: "Batman",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://static.wikia.nocookie.net/marvel_dc/images/5/50/Batman_Beyond_v.1_1.jpg",
+          price: "$2.99",
+          series: "Batman Beyond",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://static.wikia.nocookie.net/marvel_dc/images/0/0d/Batman_Superman_Vol_1_1.jpg",
+          price: "$3.99",
+          series: "Batman/Superman",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://static.wikia.nocookie.net/marvel_dc/images/c/cf/Batman_Superman_Annual_Vol_2_1.jpg",
+          price: "$4.99",
+          series: "Batman/Superman Annual",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://static.wikia.nocookie.net/marvel_dc/images/5/54/Batman_The_Joker_War_Zone_Vol_1_1.jpg",
+          price: "$5.99",
+          series: "Batman: The Joker War Zone",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://static.wikia.nocookie.net/marvel_dc/images/6/64/Batman_Three_Jokers_Collected.jpg",
+          price: "$6.99",
+          series: "Batman: Three Jokers",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://static.wikia.nocookie.net/marvel_dc/images/f/f8/Batman_White_Knight_Presents_Harley_Quinn_Vol_1_1.jpg",
+          price: "$4.99",
+          series: "Batman: White Knight Presents: Harley Quinn",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://static.wikia.nocookie.net/marvel_dc/images/c/c8/Catwoman_Vol_2_1.jpg",
+          price: "$16.99",
+          series: "Catwoman",
+          type: "graphic novel",
+        },
+      ],
+    };
+  },
+  methods: {},
 };
 
 </script>
@@ -13,12 +111,44 @@ export default {
 
   <main>
 
+    
     <figure>
 
-      <div>--Contenuto--</div>
+      <div class="jumbotron">
+
+        <div class="btn-series">current series</div>
+      </div>
 
     </figure>
 
+
+
+    <div class="container-list">
+
+
+
+      <AppComics 
+
+      v-for="(element,index) in listComics" 
+      :key="index" 
+      :propsSrc="element.thumb"
+      :propsSeries="element.series"
+      />
+      
+        <!-- <figure class="list-figure">
+
+          <img :src="element.thumb" alt="element.type">
+
+        </figure>
+
+        <h4>{{ element.series }}</h4> -->
+
+      <button>load more</button>
+
+    </div>
+    
+
+    <!-- sezione centrale azzurra -->
     <section>
 
       <ul>
@@ -68,19 +198,79 @@ export default {
 @use "../styles/partials/mixins" as *;
 
 figure{
-  height: 150px;
-  background-color: #1C1C1C;
   
-  div{
+  background-color: #1C1C1C;
+
+  .jumbotron{
+    background-image: url(../assets/img/jumbotron.jpg);
+    height: 350px;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: 0% 5%;
+    position: relative;
+
+  }
+  
+}
+
+.btn-series{
+  position: absolute;
+  padding: 15px 40px;
+  font-size: 25px;
+  text-transform: uppercase;
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+  background-color:#0282F9;
+  bottom: - 25px;
+  left: 270px;
+
+}
+
+
+.container-list{
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+    row-gap: 50px;
     width: 70%;
     margin: 0 auto;
-    height: 100%;
     color: white;
-    display: flex;
-    align-items: center;
-    font-size: 30px;
+    background-color: #1C1C1C;
+    padding-block: 50px;
+
+    button{
+      margin: 0 auto;
+      border: none;
+      background: none;
+      background-color:#0282F9;
+      padding: 15px 60px;
+      font-size: 20px;
+      text-transform: uppercase;
+      color: white;
+      font-weight: bold;
+      cursor: pointer;
+    }
+    
+  .list{
+    width: calc((100% / 6) - 20px);
+    text-transform: uppercase;
+    // .list-figure{
+    //   aspect-ratio: 1;
+    //   overflow: hidden;
+    //   margin-bottom: 10px;
+
+    //   img{
+    //   width: 100%;
+      
+    //   }
+    
+    // }
+  
+    
   }
 }
+
 
 section{
   height: 150px;
